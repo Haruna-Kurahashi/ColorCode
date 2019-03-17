d4b98dd1d004217bf8a6881ab557b11d54345841
Haruna-Kurahashi <43772045+Haruna-Kurahashi@users.noreply.github.com>
2019-03-18 03:13:47 +0900

    gallery

--- app/src/main/java/kuppieproduct/io/colorcode/MainActivity.java
+++ app/src/main/java/kuppieproduct/io/colorcode/MainActivity.java
@@ -1,9 +1,14 @@
 package kuppieproduct.io.colorcode;
 
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -12,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,11 +33,18 @@ public class MainActivity extends AppCompatActivity {
     ListView colorListView;
     FindColorAdapter adapter2;
 
+    FloatingActionButton selectImageButton;
+
+    public static int READ_REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.list_view);
+        setContentView(R.layout.choose_color);
+        listView = (ListView) findViewById(R.id.choosedcolorList);
+
+        selectImageButton = (FloatingActionButton) findViewById(R.id.addphoto);
+
         Colors = new ArrayList<Color>();
 
         adapter = new ColorAdapter(this, 0, new ArrayList<Color>());
@@ -70,10 +83,37 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                intent.setType("image/*");
+                startActivityForResult(intent, READ_REQUEST_CODE);
+            }
+        });
+
         adapter2 = new FindColorAdapter(this, 0, new ArrayList<FindColor>());
         colorListView.setAdapter(adapter2);
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    imageView.setImageBitmap(bitmap);
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     public void addColor(View view) {
         int color = ((ColorDrawable) colorPreviewView.getBackground()).getColor();
